f5ec1cad2a15b680828d2a858287885fb7d13b51
unknown <luiismoncada@gmail.com>
2016-04-20 18:15:24 -0430

    Primer Comit

---
+++ html/js/show-menu.js
@@ -0,0 +1,11 @@
+//show-menu.js
+//Muestra y oculta el menu
+$(document).ready(function () {
+
+  $(".cd-nav-trigger").click(function() {
+    
+    $(".cd-nav").toggleClass("show");
+  
+  });
+  
+});
